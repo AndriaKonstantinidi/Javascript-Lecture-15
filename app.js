c2b772edf053af01574b6b591f2e26159cee5e89
Student <student@itstep.ge>
2022-11-08 18:53:49 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// let array = [1,18,2,320,4]
+
+// for(let i = 0; i < array.length; i++){
+
+//     for(let j = 0; j < array.length; j++){
+//         if(array[i] < array[j]){
+//             tmp = array[i];
+//             array[i] = array[j];
+//             array[j] = tmp
+//         }
+//     }
+// }
+// console.log(array);
+
+// for(let i = 0; i < array.length; i++){
+//     if(array[i] > array[i + 1]){
+//         let save = array[i]
+//         array[i] = array[i + 1]
+//         array[i + 1] = save
+//         i =- 1
+//     }
+// }
+// console.log(array);
+
